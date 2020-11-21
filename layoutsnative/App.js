92206a5ad0dadad9cd1ede3e60006bfc8c0978ff
AngelLM <alm37454@gmail.com>
2020-11-21 17:36:22 +0100

    Segunda práctica de layouts terminada

--- layoutsnative/App.js
+++ layoutsnative/App.js
@@ -1,42 +1,142 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image, Text, ScrollView} from 'react-native';
 
 const App = () => {
   return (
     <>
-      <View style={styles.contenedor}>
-        <View style={styles.caja1}></View>
-        <View style={styles.caja2}></View>
-        <View style={styles.caja3}></View>
-        <View style={styles.caja4}></View>
-      </View>
+      <ScrollView>
+        
+        <View style = {{flexDirection:'row'}}>
+        <Image
+          style={styles.banner} 
+          source={ require('./assets/img/bg.jpg') }
+        />
+        </View>
+
+        <View style={styles.contenedor}>
+          <Text style={styles.titulo}>Qué hacer en París</Text>
+          <ScrollView horizontal>
+              <View>
+                <Image
+                  style={styles.ciudad} 
+                  source={ require('./assets/img/actividad1.jpg') }
+                />
+              </View>
+              <View>
+                <Image
+                  style={styles.ciudad} 
+                  source={ require('./assets/img/actividad2.jpg') }
+                />
+              </View>
+              <View>
+                <Image
+                  style={styles.ciudad} 
+                  source={ require('./assets/img/actividad3.jpg') }
+                />
+              </View>
+              <View>
+                <Image
+                  style={styles.ciudad} 
+                  source={ require('./assets/img/actividad4.jpg') }
+                />
+              </View>
+              <View>
+                <Image
+                  style={styles.ciudad} 
+                  source={ require('./assets/img/actividad5.jpg') }
+                />
+              </View>
+          </ScrollView>
+
+          <Text style={styles.titulo}>Los mejores alojamientos</Text>
+          <View>
+            <View>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/mejores1.jpg') }
+                />
+              </View>
+            <View>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/mejores2.jpg') }
+                />
+              </View>
+            <View>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/mejores3.jpg') }
+                />
+              </View>
+          </View>
+
+          <Text style={styles.titulo}>Hospedajes en LA</Text>
+            <View
+              style={styles.listado}
+            >
+              <View style={styles.listadoitem}>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/hospedaje1.jpg') }
+                />
+              </View>
+              <View style={styles.listadoitem}>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/hospedaje2.jpg') }
+                />
+              </View>
+              <View style={styles.listadoitem}>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/hospedaje3.jpg') }
+                />
+              </View>
+              <View style={styles.listadoitem}>
+                <Image
+                  style={styles.mejores} 
+                  source={ require('./assets/img/hospedaje4.jpg') }
+                />
+              </View>
+            </View>
+        </View>
+
+      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  contenedor: {
-    backgroundColor: 'cornflowerblue',
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
+  banner:{
+    height: 100,
+    flex:1
   },
-  caja1:{
-    padding:20,
-    backgroundColor: 'navy',
+  titulo:{
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginVertical: 20,
   },
-  caja2:{
-    padding:20,
-    backgroundColor: 'yellow',
+  contenedor:{
+    marginHorizontal: 10
   },
-  caja3:{
-    padding:20,
-    backgroundColor: 'green',
+  ciudad:{
+    width: 150,
+    height: 200,
+    marginRight: 10
   },
-  caja4:{
-    padding:20,
-    backgroundColor: 'teal',
+  mejores:{
+    width: '100%',
+    height: 150,
+    marginVertical: 5,
   },
+  listado:{
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  listadoitem:{
+    flexBasis:'49%'
+  }
 });
 
 export default App;
