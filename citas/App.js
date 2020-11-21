520dce9a3389b4113c3515283d7178f3a664cb88
AngelLM <alm37454@gmail.com>
2020-11-21 01:53:24 +0100

    añadidas varias funcionalidades a la aplicación

--- citas/App.js
+++ citas/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import Cita from './componentes/Cita';
+import Formulario from './componentes/formulario';
 
 const App = () => {
+
+  // definir el state de citas
+  const [citas, setCitas] = useState([
+    {id: "1", paciente: "Hook", propietario: "Juan", sintomas: "No Come"},
+    {id: "2", paciente: "Redux", propietario: "Itzel", sintomas: "No Duerme"},
+    {id: "3", paciente: "Native", propietario: "Josue", sintomas: "No Canta"},
+  ]);
+
+  // Elimina los pacientes del state
+  const eliminarPaciente = id => {
+    setCitas( (citasActuales) => {
+      return citasActuales.filter( cita => cita.id !== id)
+    })
+  }
+
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de Citas</Text>
+
+      <Formulario />
+
+      <Text style={styles.titulo}> {citas.length > 0 ? 'Administra tus citas' : 'No hay citas, agrega una'} </Text>
+            
+      <FlatList
+        data={citas}
+        renderItem={ ({item}) => <Cita item={item} eliminarPaciente={eliminarPaciente}/>}
+        keyExtractor={ cita => cita.id}
+      />
+
     </View>
   );
 };
@@ -16,10 +44,11 @@ const styles = StyleSheet.create({
   },
   titulo: {
     color: '#FFF',
-    marginTop: 40,
+    marginTop: 20,
+    marginBottom: 20,
     fontSize: 24,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
   }
 });
 
---
+++ citas/componentes/formulario.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, TextInput} from 'react-native';
+
+const Formulario = () => {
+    return(
+        <>
+        <View style={styles.formulario}>
+            <View>
+                <Text style={styles.label}>Paciente: </Text>
+                <TextInput 
+                    style = {styles.input}
+                    onChangeText={ (texto) => console.log(texto) }
+                />
+            </View>
+
+            <View>
+                <Text style={styles.label}>Dueño: </Text>
+                <TextInput 
+                    style = {styles.input}
+                    onChangeText={ (texto) => console.log(texto) }
+                />
+            </View>
+
+            <View>
+                <Text style={styles.label}>Teléfono Contacto: </Text>
+                <TextInput 
+                    style = {styles.input}
+                    onChangeText={ (texto) => console.log(texto) }
+                    keyboardType = 'numeric'
+                />
+            </View>
+
+            <View>
+                <Text style={styles.label}>Síntomas: </Text>
+                <TextInput 
+                    style = {styles.input}
+                    onChangeText={ (texto) => console.log(texto) }
+                    multiline
+                />
+            </View>
+        </View>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    formulario:{
+        backgroundColor:'#FFF',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        marginHorizontal: '2.5%'
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 16,
+        marginTop: 20,
+    },
+    input:{
+        marginTop: 10,
+        height: 50,
+        borderColor: '#e1e1e1',
+        borderWidth: 1,
+        borderStyle: 'solid',
+    }
+})
+
+export default Formulario;
+
