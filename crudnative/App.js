38f0c30dfdf74acd86573e5942002a4e9ac41e64
AngelLM <alm37454@gmail.com>
2020-11-24 20:07:52 +0100

    Iniciada app con react-native init y primeros cambios

---
+++ crudnative/App.js
@@ -0,0 +1,50 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
+
+import Inicio from './views/Inicio'
+import NuevoCliente from './views/NuevoCliente'
+import DetallesCliente from './views/DetallesCliente'
+
+const Stack = createStackNavigator();
+
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName='Inicio'  
+        >
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+          />
+          <Stack.Screen
+            name="NuevoCliente"
+            component={NuevoCliente}
+            options={{
+              title: 'Nuevo Cliente'
+            }}
+          />
+          <Stack.Screen
+            name="DetallesCliente"
+            component={DetallesCliente}
+            options={{
+              title: 'Detalles Cliente'
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
