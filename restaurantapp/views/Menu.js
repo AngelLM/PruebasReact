46f37dad700f3175223fed65fe89d697490d4497
AngelLM <alm37454@gmail.com>
2020-11-28 19:45:25 +0100

    Creada app con react-native para interaccionar con la base de datos del restaurante y hacer pedidos

---
+++ restaurantapp/views/Menu.js
@@ -0,0 +1,81 @@
+import React, { useContext, useEffect, Fragment } from 'react';
+import { StyleSheet } from 'react-native';
+import FirebaseContext from '../context/firebase/firebaseContext';
+import { 
+    Container,
+    Separator,
+    Content,
+    List,
+    ListItem,
+    Thumbnail,
+    Text,
+    Body
+ } from 'native-base';
+ import globalStyles from '../styles/global';
+
+
+
+const Menu = () => {
+
+    // Context de Firebase
+    const { menu, obtenerProductos } = useContext(FirebaseContext);
+
+    useEffect(() => {
+        obtenerProductos();
+    },[]);
+
+    const mostrarHeading = (categoria, i) => {
+        if(i>0){
+            const categoriaAnterior = menu[i-1].categoria;
+
+            if(categoriaAnterior != categoria){
+                return(
+                    <Separator>
+                        <Text>{categoria}</Text>
+                    </Separator>
+                )
+            }
+        } else {
+            return(
+                <Separator>
+                    <Text>{categoria}</Text>
+                </Separator>
+            )
+        }
+                
+
+    }
+
+    return ( 
+        <Container style={globalStyles.contenedor}>
+            <Content style={{backgroundColor:'#FFF'}}>
+                <List>
+                    {menu.map( (platillo, i) => {
+                        const { imagen, nombre, descripcion, categoria, id, precio } = platillo;
+
+                        return(
+                            <Fragment key={id}>
+                                {mostrarHeading(categoria, i)}
+                                <ListItem>
+                                    <Thumbnail large square source={{uri: imagen}} />
+                                    <Body>
+                                        <Text>{nombre}</Text>
+                                        <Text
+                                            note
+                                            numberOfLines={2}
+                                        >
+                                            {descripcion}
+                                        </Text>
+                                        <Text>Precio: {precio}€</Text>
+                                    </Body>
+                                </ListItem>
+                            </Fragment>
+                        )
+                    })}
+                </List>
+            </Content>
+        </Container>
+     );
+}
+ 
+export default Menu;
